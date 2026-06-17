4aba5b7292d698e89b44fbf03f31719444982bc5
Emmanuel Adeniran <Adeniranbanjo07@gmail.com>
2026-06-17 10:46:55 +0100

    This is the First Commit, Just Adding the Files

---
+++ src/test/java/pageObject/HomePage.java
@@ -0,0 +1,14 @@
+package pageObject;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage {
+	
+	// Constructor
+	public HomePage(WebDriver driver)
+	{
+		super(driver);
+	}
+	
+	
+}
---
+++ src/test/java/pageObject/LoginPage.java
@@ -0,0 +1,52 @@
+package pageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage {
+	
+	// Constructor
+	public LoginPage(WebDriver driver) {
+		super(driver);
+	}
+	
+	// Locators
+	@FindBy(xpath="//img[@title='ParaBank']") WebElement logoDisplay;
+	@FindBy(xpath="//input[@name='username']") WebElement txtUsername;
+	@FindBy(xpath="//input[@name='password']") WebElement txtPassword;
+	@FindBy(xpath="//input[@value='Log In']") WebElement btnLogin;
+	@FindBy(xpath="//a[normalize-space()='Forgot login info?']") WebElement btnForgotLogin;
+	@FindBy(xpath="//a[normalize-space()='Register']") WebElement btnRegister;
+	
+	// Actions
+	//1. Validate Login Page
+	public Boolean logoDisplay()
+	{
+		try
+		{
+			return(logoDisplay.isDisplayed());
+		} catch(Exception e)
+		{
+			return false;
+		}
+	}
+	
+	//2. Login Action
+	public void inputUsername(String value)
+	{
+		txtUsername.sendKeys(value);
+	}
+	
+	public void inputPassword(String value)
+	{
+		txtPassword.sendKeys(value);
+	}
+	
+	public void clickLogin()
+	{
+		btnLogin.click();
+	}
+	
+	
+}
